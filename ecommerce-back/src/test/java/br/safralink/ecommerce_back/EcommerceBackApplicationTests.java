5d04a282fb33aecb2e5d66bfdfa06d63257dc298
Leonardo Jose <devleonardopereira@gmail.com>
2026-08-18 23:34:59 +0000

    Criação do projeto

---
+++ ecommerce-back/src/test/java/br/safralink/ecommerce_back/EcommerceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.safralink.ecommerce_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
